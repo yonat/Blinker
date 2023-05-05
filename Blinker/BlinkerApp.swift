147f48adf2edd12dd0610de0f259b98b2afa0f38
Yonat Sharon <yonat@ootips.org>
2023-05-05 11:08:47 +0300

    Initial Commit

---
+++ Blinker/BlinkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlinkerApp.swift
+//  Blinker
+//
+//  Created by Yonat Sharon on 05/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct BlinkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
