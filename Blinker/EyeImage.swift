3b50a3d88b9928f26fdb542f1f7e79779a38b5c9
Yonat Sharon <yonat@ootips.org>
2023-05-05 11:36:34 +0300

    manual fixing

--- Blinker/EyeImage.swift
+++ Blinker/EyeImage.swift
@@ -8,52 +8,56 @@
 import SwiftUI
 
 struct EyeImage: View {
-    @State var isBlinking = false
+    @State var blinkAmount: Double = 1
 
     var body: some View {
-        ZStack {
-            // Eyelids
-            Path { path in
-                let height = 15.0
-                let yOffset = isBlinking ? height : 0.0
-                path.move(to: CGPoint(x: -35, y: -yOffset))
-                path.addQuadCurve(to: CGPoint(x: 35, y: -yOffset), control: CGPoint(x: 0, y: -height))
-                path.closeSubpath()
+        GeometryReader { geometry in
+            ZStack {
+                // Sclera
+                Path { path in
+                    let midHeight = geometry.size.height / 2
+                    let midWidth = geometry.size.width / 2
+                    let startPoint = CGPoint(x: 0, y: midHeight)
+                    let endPoint = CGPoint(x: geometry.size.width, y: midHeight)
+                    let scleraHeight = midHeight * 0.5
+                    path.move(to: startPoint)
+                    path.addQuadCurve(
+                        to: endPoint,
+                        control: CGPoint(x: midWidth, y: -scleraHeight))
+                    path.addQuadCurve(
+                        to: startPoint,
+                        control: CGPoint(x: midWidth, y: midHeight * 2 + scleraHeight)
+                    )
+                    path.closeSubpath()
+                }
+                .fill(Color.white)
+                .shadow(radius: 5, y: 5)
+                // Iris
+                Circle()
+                    .fill(Color.teal)
+                    .frame(width: geometry.size.height * 0.7, height: geometry.size.height * 0.7)
+                // Pupil
+                Circle()
+                    .fill(Color.black)
+                    .frame(width: geometry.size.height / 4, height: geometry.size.height / 4)
+                    .shadow(radius: 3, y: 3)
+                // Glare
+                Circle()
+                    .fill(Color.white.opacity(0.5))
+                    .frame(width: geometry.size.height / 5, height: geometry.size.height / 5)
+                    .offset(x: geometry.size.height / 5, y: -geometry.size.height / 5)
+                // Reflection
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: geometry.size.height / 10, height: geometry.size.height / 10)
+                    .offset(x: -geometry.size.height / 10, y: -geometry.size.height / 10)
             }
-            .fill(Color.red)
-            .shadow(radius: 3, y: 3)
-            // Iris and Pupil
-            Circle()
-                .fill(Color.blue)
-                .frame(width: 50, height: 50)
-            Circle()
-                .fill(Color.black)
-                .frame(width: 30, height: 30)
-                .shadow(radius: 3, y: 3)
-            // Highlight
-            Circle()
-                .fill(Color.white)
-                .frame(width: 10, height: 10)
-                .offset(x: 15, y: -15)
-            // Reflection
-            Circle()
-                .fill(Color.white)
-                .frame(width: 10, height: 10)
-                .offset(x: -10, y: -10)
+            .scaleEffect(x: 1, y: blinkAmount)
+            .animation(.default.repeatForever(autoreverses: true), value: blinkAmount)
         }
-        .frame(width: 70, height: 70)
-        .animation(.easeInOut(duration: 0.3), value: isBlinking)
-        .onAppear {
-            Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { _ in
-                withAnimation(.easeInOut(duration: 0.3)) {
-                    isBlinking = true
-                }
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                    withAnimation(.easeInOut(duration: 0.3)) {
-                        isBlinking = false
-                    }
-                }
-            }
+        .frame(width: 100, height: 80)
+        .task {
+            blinkAmount = 0
         }
     }
 }
